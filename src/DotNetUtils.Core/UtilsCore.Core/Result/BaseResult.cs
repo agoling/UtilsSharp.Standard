851f25a8021b48ced1e9902b142f469a33e4f664
122473843 <122473843@qq.com>
2019-08-29 14:43:22 +0800

    同步

--- src/DotNetUtils.Core/UtilsCore.Core/Result/BaseResult.cs
+++ src/DotNetUtils.Core/UtilsCore.Core/Result/BaseResult.cs
@@ -63,7 +63,7 @@ namespace UtilsCore.Core.Result
         public string Msg { get; set; } = "请求成功";
 
         /// <summary>
-        /// 设置错误消息
+        /// 设置错误提示
         /// </summary>
         /// <param name="result">基础结果信息</param>
         public void SetError(BaseInfoResult result)

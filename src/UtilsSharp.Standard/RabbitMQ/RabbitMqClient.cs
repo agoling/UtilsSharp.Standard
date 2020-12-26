fddba4d7006b39aa45cbac63245dd326b6e7cc0b
122473843 <122473843@qq.com>
2020-12-26 17:06:55 +0800

    优化rabbitmq性能

--- src/UtilsSharp.Standard/RabbitMQ/RabbitMqClient.cs
+++ src/UtilsSharp.Standard/RabbitMQ/RabbitMqClient.cs
@@ -7,97 +7,68 @@ namespace RabbitMQ
     /// <summary>
     /// RabbitMq客户端
     /// </summary>
-    public sealed class RabbitMqClient
+    public class RabbitMqClient
     {
-        private static ConcurrentDictionary<string, Tuple<string, string, string>> _cdic;
-        private static RabbitMqClient _instance;
-        private static readonly object InstanceLock = new object();
-
+        private static ConcurrentDictionary<string, Tuple<string, string, string>> _cdic=new ConcurrentDictionary<string, Tuple<string, string, string>>();
+        private static RabbitMqHelper _rabbitMqHelper = new RabbitMqHelper();
+        
         private RabbitMqClient()
         {
 
         }
 
         /// <summary>
-        ///  RabbitMq实例
+        /// 生产者（发送消息）
         /// </summary>
-        public static RabbitMqClient GetInstance()
+        public static void Send<T>(string businessName, T content) where T : class
         {
-            switch (_instance)
-            {
-                case null:
-                    {
-                        lock (InstanceLock)
-                        {
-                            if (_instance == null)
-                            {
-                                _instance = new RabbitMqClient();
-                            }
-                        }
-                        break;
-                    }
-            }
-            return _instance;
+            var t = Get(businessName);
+            _rabbitMqHelper.Send(t.Item1, t.Item2, content);
         }
 
         /// <summary>
         /// 生产者（发送消息）
         /// </summary>
-        public void Send<T>(string businessName, T content) where T : class
+        public static void Send<T>(string businessName, List<T> contents) where T : class
         {
-            var rabbitMqHelper = new RabbitMqHelper();
             var t = Get(businessName);
-            rabbitMqHelper.Send(t.Item1, t.Item2, content);
+            _rabbitMqHelper.Send(t.Item1, t.Item2, contents);
         }
 
         /// <summary>
         /// 生产者（发送消息）
         /// </summary>
-        public void Send<T>(string businessName, List<T> contents) where T : class
+        public static void Send<T>(string businessName, T content, int expiration) where T : class
         {
-            var rabbitMqHelper = new RabbitMqHelper();
             var t = Get(businessName);
-            rabbitMqHelper.Send(t.Item1, t.Item2, contents);
+            _rabbitMqHelper.Send(t.Item1, t.Item2, content, expiration);
         }
 
         /// <summary>
         /// 生产者（发送消息）
         /// </summary>
-        public void Send<T>(string businessName, T content, int expiration) where T : class
+        public static void Send<T>(string businessName, List<T> contents, int expiration) where T : class
         {
-            var rabbitMqHelper = new RabbitMqHelper();
             var t = Get(businessName);
-            rabbitMqHelper.Send(t.Item1, t.Item2, content, expiration);
-        }
-
-        /// <summary>
-        /// 生产者（发送消息）
-        /// </summary>
-        public void Send<T>(string businessName, List<T> contents, int expiration) where T : class
-        {
-            var rabbitMqHelper = new RabbitMqHelper();
-            var t = Get(businessName);
-            rabbitMqHelper.Send(t.Item1, t.Item2, contents, expiration);
+            _rabbitMqHelper.Send(t.Item1, t.Item2, contents, expiration);
         }
 
         /// <summary>
         /// 消费者（消费消息）
         /// </summary>
-        public void Received(string businessName, Action<string> callback, string exchangeType = "direct", Action errorCallback = null)
+        public static void Received(string businessName, Action<string> callback, string exchangeType = "direct", Action errorCallback = null)
         {
-            var rabbitMqHelper = new RabbitMqHelper();
             var t = Get(businessName);
-            rabbitMqHelper.Received(t.Item1, t.Item2, t.Item3, callback, exchangeType, errorCallback);
+            _rabbitMqHelper.Received(t.Item1, t.Item2, t.Item3, callback, exchangeType, errorCallback);
         }
 
         /// <summary>
         /// 批量消费者（消费消息）
         /// </summary>
-        public void BatchReceived(string businessName, Action<List<string>> callback, int batchCount = 50, Action errorCallback = null)
+        public static void BatchReceived(string businessName, Action<List<string>> callback, int batchCount = 50, Action errorCallback = null)
         {
-            var rabbitMqHelper = new RabbitMqHelper();
             var t = Get(businessName);
-            rabbitMqHelper.BatchReceived(t.Item3, callback, batchCount, errorCallback);
+            _rabbitMqHelper.BatchReceived(t.Item3, callback, batchCount, errorCallback);
         }
 
         /// <summary>
@@ -109,6 +80,10 @@ namespace RabbitMQ
         private static Tuple<string, string, string> Get(string businessName)
         {
             businessName = string.IsNullOrEmpty(businessName) ? "DefaultBusiness" : businessName.Trim('.');
+            if (_rabbitMqHelper == null)
+            {
+                _rabbitMqHelper = new RabbitMqHelper();
+            }
             if (_cdic == null)
             {
                 _cdic = new ConcurrentDictionary<string, Tuple<string, string, string>>();
@@ -120,13 +95,12 @@ namespace RabbitMQ
             var exchangeName = $"{businessName}.Exchange";
             var routingKey = $"{businessName}.RoutingKey";
             var queueName = $"{businessName}.Queue";
-            var rabbitMqHelper = new RabbitMqHelper();
             //申明交换机
-            rabbitMqHelper.ExchangeDeclare(exchangeName);
+            _rabbitMqHelper.ExchangeDeclare(exchangeName);
             //申明队列
-            rabbitMqHelper.QueueDeclare(queueName);
+            _rabbitMqHelper.QueueDeclare(queueName);
             //绑定
-            rabbitMqHelper.QueueBind(queueName, exchangeName, routingKey);
+            _rabbitMqHelper.QueueBind(queueName, exchangeName, routingKey);
             var tuple = new Tuple<string, string, string>(exchangeName, routingKey, queueName);
             _cdic.TryAdd(businessName, tuple);
             return tuple;

92b3a32e405060b203d754f03811dd83ee4b78b2
122473843 <122473843@qq.com>
2021-05-17 12:40:19 +0800

    调整

---
+++ src/UtilsSharp.Standard/UnitTestProject/JsonXmlHelperTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UtilsSharp;
+
+namespace UnitTestProjectNetCore
+{
+
+    [TestClass]
+    public class JsonXmlHelperTest
+    {
+        [TestMethod]
+        public void JsonXml()
+        {
+            var aa = JsonXmlHelper.XmlToJson("<Test><Name>Test class</Name><X>100</X><Y>200</Y></Test>");
+            var bb= JsonXmlHelper.JsonToXml("{\"Test\":{\"Name\":\"Test class\",\"X\":\"100\",\"Y\":\"200\"}}");
+          
+        }
+    }
+}
---
+++ src/UtilsSharp.Standard/UtilsSharp/JsonXmlHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace UtilsSharp
+{
+    /// <summary>
+    /// JsonXml帮助类
+    /// </summary>
+    public class JsonXmlHelper
+    {
+        /// <summary>
+        /// json转xml
+        /// </summary>
+        /// <param name="json">json字符串</param>
+        /// <returns></returns>
+        public static string JsonToXml(string json)
+        {
+            var doc = JsonConvert.DeserializeXmlNode(json);
+            var xml = doc.InnerXml;
+            return xml;
+        }
+
+        /// <summary>
+        /// xml转json
+        /// </summary>
+        /// <param name="xml">xml字符串</param>
+        /// <returns></returns>
+        public static string XmlToJson(string xml)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(xml);
+            var json = JsonConvert.SerializeXmlNode(doc);
+            return json;
+        }
+
+    }
+}

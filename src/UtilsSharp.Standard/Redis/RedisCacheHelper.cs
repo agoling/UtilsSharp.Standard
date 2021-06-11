65caea1dc8842dd21782bebfa10cf973f5245f05
122473843 <122473843@qq.com>
2021-06-11 15:07:07 +0800

    redis升级支持切库

--- src/UtilsSharp.Standard/Redis/RedisCacheHelper.cs
+++ src/UtilsSharp.Standard/Redis/RedisCacheHelper.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using CSRedis;
 
 namespace Redis
 {
     /// <summary>
-    /// Redis缓存
+    /// RedisCacheHelper
     /// </summary>
-    public class RedisCache
+    public abstract class RedisCacheHelper : RedisCacheHelper<RedisCacheHelper> { }
+
+    /// <summary>
+    /// Redis缓存帮助类
+    /// </summary>
+    public abstract class RedisCacheHelper<TMark>
     {
+        private static CSRedisClient _instance;
+
+        /// <summary>
+        /// CSRedisClient 静态实例，使用前请初始化
+        /// RedisHelper.Initialization(new CSRedis.CSRedisClient(\"127.0.0.1:6379,password=123,defaultDatabase=13,poolsize=50,ssl=false,writeBuffer=10240,prefix=key前辍\"))
+        /// </summary>
+        public static CSRedisClient Instance
+        {
+            get
+            {
+                if (_instance == null) throw new Exception("使用前请初始化 RedisHelper.Initialization(new CSRedis.CSRedisClient(\"127.0.0.1:6379,password=123,defaultDatabase=13,poolsize=50,ssl=false,writeBuffer=10240,prefix=key前辍\"));");
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 初始化csredis静态访问类
+        /// RedisHelper.Initialization(new CSRedis.CSRedisClient(\"127.0.0.1:6379,password=123,defaultDatabase=13,poolsize=50,ssl=false,writeBuffer=10240,prefix=key前辍\"))
+        /// </summary>
+        /// <param name="csredis"></param>
+        public static void Initialization(CSRedisClient csredis)
+        {
+            _instance = csredis;
+        }
+
         /// <summary>
         /// 添加缓存
         /// </summary>
@@ -16,10 +47,7 @@ namespace Redis
         /// <param name="value">缓存数据</param>
         /// <param name="expireSeconds">缓存时间(秒)</param>
         /// <returns>是否成功</returns>
-        public static bool Set(string key, object value, int expireSeconds = -1)
-        {
-            return RedisHelper.Set(key, value, expireSeconds);
-        }
+        public static bool Set(string key, object value, int expireSeconds = -1)=> Instance.Set(key, value, expireSeconds);
 
         /// <summary>
         /// 添加缓存
@@ -28,19 +56,13 @@ namespace Redis
         /// <param name="value">缓存数据</param>
         /// <param name="expire">缓存时间戳</param>
         /// <returns>是否成功</returns>
-        public static bool Set(string key, object value, TimeSpan expire)
-        {
-            return RedisHelper.Set(key, value, expire);
-        }
+        public static bool Set(string key, object value, TimeSpan expire)=> Instance.Set(key, value, expire);
 
         /// <summary>
         /// 移除缓存
         /// </summary>
         /// <param name="keys">缓存keys</param>
-        public static long Remove(params string[] keys)
-        {
-            return RedisHelper.Del(keys);
-        }
+        public static long Remove(params string[] keys)=> Instance.Del(keys);
 
         /// <summary>
         /// 移除缓存(pattern方式)
@@ -48,13 +70,13 @@ namespace Redis
         /// <param name="pattern">如：test*</param>
         public static long RemoveByPattern(string pattern)
         {
-            var keys = RedisHelper.Keys(pattern);
+            var keys = Instance.Keys(pattern);
             var newKeys = new List<string>();
             if (keys != null && keys.Length > 0)
             {
                 newKeys.AddRange(from item in keys let index = item.IndexOf(":", StringComparison.Ordinal) select item.Substring(index + 1));
             }
-            return RedisHelper.Del(newKeys.ToArray());
+            return Instance.Del(newKeys.ToArray());
         }
 
         /// <summary>
@@ -62,20 +84,14 @@ namespace Redis
         /// </summary>
         /// <param name="key">缓存key</param>
         /// <returns >是否存在</returns>
-        public static bool IsExists(string key)
-        {
-            return RedisHelper.Exists(key);
-        }
+        public static bool IsExists(string key)=> Instance.Exists(key);
 
         /// <summary>
         /// 获取缓存
         /// </summary>
         /// <param name="key">缓存Key</param>
         /// <returns>缓存数据</returns>
-        public static string Get(string key)
-        {
-            return RedisHelper.Get(key);
-        }
+        public static string Get(string key)=> Instance.Get(key);
 
         /// <summary>
         /// 获取缓存
@@ -83,10 +99,7 @@ namespace Redis
         /// <typeparam name="T">缓存数据类型</typeparam>
         /// <param name="key">缓存Key</param>
         /// <returns>缓存数据</returns>
-        public static T Get<T>(string key)
-        {
-            return RedisHelper.Get<T>(key);
-        }
+        public static T Get<T>(string key)=> Instance.Get<T>(key);
 
         /// <summary>
         /// 获取缓存(pattern方式)
@@ -95,7 +108,7 @@ namespace Redis
         /// <returns>缓存数据</returns>
         public static string[] GetByPattern(string pattern)
         {
-            var keys = RedisHelper.Keys(pattern);
+            var keys = Instance.Keys(pattern);
             var newKeys = new List<string>();
             if (keys != null && keys.Length > 0)
             {
--- src/UtilsSharp.Standard/Redis/RedisManager.cs
+++ src/UtilsSharp.Standard/Redis/RedisManager.cs
@@ -15,9 +15,50 @@ namespace Redis
         /// </summary>
         public static void Register()
         {
+            var csRedis = new CSRedisClient(RedisConfig.RedisConnection);
             //初始化 RedisHelper
-            CSRedisClient csredis = new CSRedisClient(RedisConfig.RedisConnection);
-            RedisHelper.Initialization(csredis);
+            RedisHelper.Initialization(csRedis);
+            //初始化 RedisCacheHelper
+            RedisCacheHelper.Initialization(csRedis);
+        }
+
+        /// <summary>
+        /// 注册
+        /// </summary>
+        /// <param name="csRedis">csRedis</param>
+        public static void Register(CSRedisClient csRedis)
+        {
+            //初始化 RedisHelper
+            RedisHelper.Initialization(csRedis);
+            //初始化 RedisCacheHelper
+            RedisCacheHelper.Initialization(csRedis);
+        }
+
+        /// <summary>
+        /// 获取多数据库CsRedisClient客户端
+        /// </summary>
+        /// <param name="redisConnection">redis连接串，无需包含：defaultDatabase</param>
+        /// <param name="dbCount">数据库数量</param>
+        /// <returns></returns>
+        public static CSRedisClient[] GetMultipleDbCsRedisClient(string redisConnection, int dbCount=1)
+        {
+            if (string.IsNullOrEmpty(redisConnection))
+            {
+                throw new Exception("连接串不能为空");
+            }
+            if (redisConnection.ToLower().Contains("defaultdatabase"))
+            {
+                throw new Exception("连接串中不能含有defaultDatabase");
+            }
+            if (dbCount == 0)
+            {
+                throw new Exception("数据库总数不能为0");
+            }
+            redisConnection = redisConnection.TrimEnd(',');
+            var csRedisArray = new CSRedisClient[dbCount]; //Singleton
+            for (var db = 0; db < csRedisArray.Length; db++)
+                csRedisArray[db] = new CSRedisClient(redisConnection + ",defaultDatabase=" + db);
+            return csRedisArray;
         }
     }
 }

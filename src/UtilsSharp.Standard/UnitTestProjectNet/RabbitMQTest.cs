80553e54aca51aee43464d58595c21b0be87fe7d
122473843 <122473843@qq.com>
2021-08-11 17:56:26 +0800

    rabbitMq NET461 版本测试

---
+++ src/UtilsSharp.Standard/UnitTestProjectNet/RabbitMQTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OptionConfig;
+using RabbitMQ;
+
+namespace UnitTestProjectNet
+{
+    [TestClass]
+    public class RabbitMQTest
+    {
+        [TestMethod]
+        public void Send()
+        {
+            RabbitMqConfig.RabbitMqSetting=new RabbitMqSetting();
+            RabbitMqConfig.RabbitMqSetting.RabbitMqConnection = "amqp://admin:123456@192.168.0.144:5672";
+            RabbitMqManager.Register();
+
+            string businessName = "test_mq";
+            List<string> mqTests = new List<string>();
+            mqTests.Add("测试11");
+            mqTests.Add("测试21");
+            mqTests.Add("测试31");
+            RabbitMqHelper.Send(businessName, mqTests);
+
+        }
+
+
+        [TestMethod]
+        public void GetMessage()
+        {
+            RabbitMqConfig.RabbitMqSetting = new RabbitMqSetting();
+            RabbitMqConfig.RabbitMqSetting.RabbitMqConnection = "amqp://alimquser:sfayxgtxkmuh@192.168.0.144:5672";
+            RabbitMqManager.Register();
+
+            string businessName = "test_mq";
+            //var r=RabbitMqHelper.GetMessage(businessName, true);
+            //var r1 = RabbitMqHelper.GetMessage(businessName, true);
+
+            //var r2 = RabbitMqHelper.GetMessage(businessName, false);
+            RabbitMqHelper.Received(businessName, fun, "direct", (string e) =>
+            {
+                var cc =e;
+
+            });
+
+
+            //var r3 = RabbitMqHelper.GetMessage(businessName, false);
+
+            //RabbitMqHelper.BasicAck(r3.DeliveryTag,true);
+            Thread.Sleep(6000);
+        }
+
+        public void fun(string aa)
+        {
+            var bb = aa;
+        }
+
+    }
+
+}

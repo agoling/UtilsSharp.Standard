58ed9cd77e4b0228173f8ad1050e8b5d2ffe233e
ygl <122473843@qq.com>
2022-05-17 19:39:54 +0800

    UtilsSharp.DotNetCore 增加AutoMapping

--- src/UtilsSharp.Standard/DotNetCore/Autofac/AutofacContainer.cs
+++ src/UtilsSharp.Standard/DotNetCore/Autofac/AutofacContainer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Autofac;
 using Autofac.Extras.DynamicProxy;
+using AutoMapper;
 using UtilsSharp;
 using UtilsSharp.Dependency;
 
@@ -17,6 +18,9 @@ namespace DotNetCore.Autofac
 
         public static ILifetimeScope Current => _lifetimeScope;
 
+        /// <summary>
+        /// 依赖注入初始化
+        /// </summary>
         public static void Register()
         {
             var builder = new ContainerBuilder();
@@ -24,9 +28,28 @@ namespace DotNetCore.Autofac
             builder.RegisterAssemblyTypes(enumerables.ToArray()).Where(t => typeof(ISingletonDependency).IsAssignableFrom(t) && typeof(ISingletonDependency) != t).AsImplementedInterfaces().SingleInstance().EnableInterfaceInterceptors();//注册类
             builder.RegisterAssemblyTypes(enumerables.ToArray()).Where(t => typeof(ITransientDependency).IsAssignableFrom(t) && typeof(ITransientDependency) != t).AsImplementedInterfaces().InstancePerDependency().EnableInterfaceInterceptors();//注册类
             builder.RegisterAssemblyTypes(enumerables.ToArray()).Where(t => typeof(IUnitOfWorkDependency).IsAssignableFrom(t) && typeof(IUnitOfWorkDependency) != t).AsImplementedInterfaces().InstancePerLifetimeScope().EnableInterfaceInterceptors();//注册类
+            builder.Register(context => new MapperConfiguration(cfg =>
+                {
+                    cfg.AddMaps(enumerables);
+                }
+            )).AsSelf().SingleInstance();
+
+            builder.Register(c =>
+                {
+                    //This resolves a new context that can be used later.
+                    var context = c.Resolve<IComponentContext>();
+                    var config = context.Resolve<MapperConfiguration>();
+                    return config.CreateMapper(context.Resolve);
+                })
+                .As<IMapper>()
+                .InstancePerLifetimeScope();
             _lifetimeScope = builder.Build();
         }
 
+        /// <summary>
+        /// 依赖注入初始化
+        /// </summary>
+        /// <param name="action">回调</param>
         public static void Register(Action<ContainerBuilder> action)
         {
             var builder = new ContainerBuilder();
@@ -34,6 +57,21 @@ namespace DotNetCore.Autofac
             builder.RegisterAssemblyTypes(enumerables.ToArray()).Where(t => typeof(ISingletonDependency).IsAssignableFrom(t) && typeof(ISingletonDependency) != t).AsImplementedInterfaces().SingleInstance().EnableInterfaceInterceptors();//注册类
             builder.RegisterAssemblyTypes(enumerables.ToArray()).Where(t => typeof(ITransientDependency).IsAssignableFrom(t) && typeof(ITransientDependency) != t).AsImplementedInterfaces().InstancePerDependency().EnableInterfaceInterceptors();//注册类
             builder.RegisterAssemblyTypes(enumerables.ToArray()).Where(t => typeof(IUnitOfWorkDependency).IsAssignableFrom(t) && typeof(IUnitOfWorkDependency) != t).AsImplementedInterfaces().InstancePerLifetimeScope().EnableInterfaceInterceptors();//注册类
+            builder.Register(context => new MapperConfiguration(cfg =>
+                {
+                    cfg.AddMaps(enumerables);
+                }
+            )).AsSelf().SingleInstance();
+
+            builder.Register(c =>
+                {
+                    //This resolves a new context that can be used later.
+                    var context = c.Resolve<IComponentContext>();
+                    var config = context.Resolve<MapperConfiguration>();
+                    return config.CreateMapper(context.Resolve);
+                })
+                .As<IMapper>()
+                .InstancePerLifetimeScope();
             action(builder);
             _lifetimeScope = builder.Build();
         }
